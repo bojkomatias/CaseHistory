b4a21f332779097a1a0a4915e607853a0651be07
Matias Bojko <bojko.matias@gmail.com>
2021-02-19 00:12:40 -0300

    probando

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import axios from 'axios';
 
 
 export default function Home() {
+
+  axios.get('http://localhost:5000/users/').then( res => console.log(res))
+
   return (
     <div className={styles.container}>
       <Head>
